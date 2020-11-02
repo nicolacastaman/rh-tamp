57284fdea34b1df88ea7493feba008891f02a211
Nicola Castaman <nicolacastaman@gmail.com>
2020-11-02 10:21:13 +0100

    Added LocatedIn in KnowledgeBase

--- moveit_tmp/src/knowledge_base.cpp
+++ moveit_tmp/src/knowledge_base.cpp
@@ -11,6 +11,7 @@ KnowledgeBase::KnowledgeBase()
   leave_clean_ = std::make_shared<LeaveClean>(this);
   loaded_ = std::make_shared<Loaded>(this);
   near_ = std::make_shared<Near>(this);
+  located_in_ = std::make_shared<LocatedIn>(this);
 }
 
 KnowledgeBase::KnowledgeBase(const KnowledgeBase& kb)
@@ -21,18 +22,26 @@ KnowledgeBase::KnowledgeBase(const KnowledgeBase& kb)
       leave_clean_(std::make_shared<LeaveClean>(*kb.leave_clean_)),
       loaded_(std::make_shared<Loaded>(*kb.loaded_)),
       near_(std::make_shared<Near>(*kb.near_)),
+      located_in_(std::make_shared<LocatedIn>(*kb.located_in_))
 {
 }
 
 std::string KnowledgeBase::getPDDL()
 {
+  return getObjectsPDDL(true) + getInitPDDL(true);
+}
+
+std::string KnowledgeBase::getObjectsPDDL(bool header)
+{
   std::string pddl;
 
-  pddl += "(:objects";
+  if (header)
+    pddl += "(:objects";
 
   std::string movable_;
   std::string fixed_;
   std::string surface_;
+  std::string place_;
 
   for (auto const& object : objects_)
   {
@@ -48,12 +57,36 @@ std::string KnowledgeBase::getPDDL()
     {
       surface_ += " " + object.id;
     }
+    else if (object.type == PLACE)
+    {
+      place_ += " " + object.id;
+    }
   }
-  pddl += movable_ + " - movable";
-  pddl += fixed_ + " - fixed";
-  pddl += surface_ + " - surface";
 
-  pddl += ")(:init";
+  if (movable_ != "")
+    pddl += movable_ + " - movable";
+
+  if (fixed_ != "")
+    pddl += fixed_ + " - fixed";
+
+  if (surface_ != "")
+    pddl += surface_ + " - surface";
+
+  if (place_ != "")
+    pddl += place_ + " - place";
+
+  if (header)
+    pddl += ")";
+
+  return pddl;
+}
+
+std::string KnowledgeBase::getInitPDDL(bool header)
+{
+  std::string pddl;
+
+  if (header)
+    pddl += "(:init";
 
   pddl += obstruct_->getPDDL();
 
@@ -69,7 +102,18 @@ std::string KnowledgeBase::getPDDL()
 
   pddl += near_->getPDDL();
 
-  pddl += ")";
+  pddl += located_in_->getPDDL();
+
+  for (auto const& object : objects_)
+  {
+    if (object.type == MOVABLE)
+      pddl += "(= (weight " + object.id + ") " + std::to_string(object.weight) +
+              ")";
+  }
+
+  if (header)
+    pddl += ")";
+
   return pddl;
 }
 
@@ -120,6 +164,9 @@ void KnowledgeBase::removeObject(const std::string object_id)
   obstruct_->removeObject(object_id);
   is_stackable_->removeObject(object_id);
   leave_clean_->removeObject(object_id);
+  loaded_->removeObject(object_id);
+  near_->removeObject(object_id);
+  located_in_->removeObject(object_id);
 
   objects_.erase(findObjectById(object_id));
 }
@@ -148,9 +195,17 @@ std::shared_ptr<KnowledgeBase::LeaveClean> KnowledgeBase::getLeaveClean()
   return leave_clean_;
 }
 
-std::shared_ptr<KnowledgeBase::Loaded> KnowledgeBase::getLoaded() { return loaded_; }
+std::shared_ptr<KnowledgeBase::Loaded> KnowledgeBase::getLoaded()
+{
+  return loaded_;
+}
 
 std::shared_ptr<KnowledgeBase::Near> KnowledgeBase::getNear() { return near_; }
+
+std::shared_ptr<KnowledgeBase::LocatedIn> KnowledgeBase::getLocatedIn()
+{
+  return located_in_;
+}
 
 // On Fact //
 
@@ -344,8 +399,7 @@ std::string KnowledgeBase::LeaveClean::getPDDL()
 
   for (auto const& leave_clean : leave_clean_)
   {
-    pddl +=
-        "(leave-free " + leave_clean.first + " " + leave_clean.second + ")";
+    pddl += "(leave-free " + leave_clean.first + " " + leave_clean.second + ")";
   }
 
   return pddl;
@@ -366,7 +420,8 @@ std::string KnowledgeBase::Loaded::getPDDL()
 
   for (auto const& loaded : loaded_)
   {
-    pddl += "(loaded " + loaded + ")";
+    if (loaded != "") // TODO improve
+      pddl += "(loaded " + loaded + ")";
   }
   return pddl;
 }
@@ -401,5 +456,58 @@ std::string KnowledgeBase::Near::getPDDL()
 }
 
 void KnowledgeBase::Near::reset() { near_.clear(); }
+
+// LocatedIn Fact //
+
+void KnowledgeBase::LocatedIn::add(const std::string object_1,
+                                   const std::string object_2)
+{
+  // TODO evitare un oggetto sopra a 2 oggetti
+  removeObject(object_1); // replace the object knowledge
+  located_in_.push_back(std::make_pair(object_1, object_2));
+}
+
+std::string
+KnowledgeBase::LocatedIn::getLocationById(const std::string object_id)
+{
+
+  auto it = std::find_if(
+      located_in_.begin(), located_in_.end(),
+      [object_id](const std::pair<std::string, std::string>& in) -> bool {
+        return in.first == object_id;
+      });
+
+  if (it != located_in_.end())
+  {
+    return it->second;
+  }
+
+  return "";
+}
+
+void KnowledgeBase::LocatedIn::removeObject(const std::string object_id)
+{
+  for (auto it = located_in_.begin(); it != located_in_.end(); ++it)
+  {
+    if (it->first == object_id || it->second == object_id)
+    {
+      located_in_.erase(it--); // decrese iterator to match new vector size
+    }
+  }
+}
+
+std::string KnowledgeBase::LocatedIn::getPDDL()
+{
+  std::string pddl;
+
+  for (auto const& located_in : located_in_)
+  {
+    pddl += "(located-in " + located_in.first + " " + located_in.second + ")";
+  }
+
+  return pddl;
+}
+
+void KnowledgeBase::LocatedIn::reset() { located_in_.clear(); }
 
 } // namespace moveit_tmp
